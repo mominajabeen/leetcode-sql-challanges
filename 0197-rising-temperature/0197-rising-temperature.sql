e22c465ec79213d2916cd2ff43ec6425696cab52
Momina Jabeen <momina.jabeen8@gmail.com>
2026-07-02 18:42:22 -0700

    Time: 1009 ms (5%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,19 +1,13 @@
-WITH PreviousWeatherData AS
-(
-    SELECT 
-        id,
-        recordDate,
-        temperature,
-        LAG(temperature, 1) OVER (ORDER BY recordDate) AS PreviousTemperature,
-        LAG(recordDate, 1) OVER (ORDER BY recordDate) AS PreviousRecordDAte
-        FROM
-            Weather
-)
-SELECT
-    id
+SELECT 
+    w1.id
 FROM
-    PreviousWeatherData
+    Weather w1
 WHERE
-    temperature > PreviousTemperature
-AND
-    recordDate = DATE_ADD(PreviousRecordDate, INTERVAL 1 DAY);
+    w1.temperature > (
+        SELECT
+            w2.temperature
+        FROM
+            Weather w2
+        WHERE
+            w2.recordDate = DATE_SUB(w1.recordDate, INTERVAL 1 DAY)
+    );
